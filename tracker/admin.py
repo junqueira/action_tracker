2189f3d76a588bc8482fa19278aa0dd57f30fd2d
valdergallo <valdergallo@gmail.com>
2014-09-04 17:29:08 -0300

    new tracker models

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Folder, Project, Task, Comment
 
-# Register your models here.
+admin.site.register(Folder)
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Comment)
--- tracker/models.py
+++ tracker/models.py
@@ -1,38 +1,58 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Task(models.Model):
-    #log fields
+
+class BaseModelDate(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
 
-    #business fields
+    class Meta:
+        abstract = True
+
+
+class Project(BaseModelDate):
+    created_by = models.ForeignKey(User)
+    name = models.CharField(max_length=250, null=True, blank=True)
+    assigneds = models.ManyToManyField(User, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Folder(BaseModelDate):
+    created_by = models.ForeignKey(User)
+    parent = models.ForeignKey('self', null=True, blank=True)
+    code = models.CharField(max_length=150, null=True, blank=True)
+    name = models.CharField(max_length=250, null=True, blank=True)
+    order = models.CommaSeparatedIntegerField(default=0)
+    assigneds = models.ManyToManyField(User, null=True, blank=True)
+
+    def __str__(self):
+        return self.code
+
+
+class TaskToUser(models.Model):
+    user = models.ForeignKey(User)
+    task = models.ForeignKey('Task')
+
+
+class Task(BaseModelDate):
+    created_by = models.ForeignKey('User')
     title = models.CharField(max_length=200, null=False)
     description = models.TextField(null=False)
     expected_finish_date = models.DateTimeField(auto_now_add=True)
     finish_date = models.DateTimeField(auto_now=True)
 
+    owners = models.ManyToManyField('self', through=TaskToUser)
+    assigneds = models.ManyToManyField('self', null=True, blank=True, through=TaskToUser)
+    approvals = models.ManyToManyField('self', null=True, blank=True, through=TaskToUser)
+    stackholders = models.ManyToManyField('self', null=True, blank=True, through=TaskToUser)
 
-class TaskOwner(models.Model):
-    #log fields
-    creation_date = models.DateTimeField(auto_now_add=True)
-
-    user = models.ForeignKey(User)
-    task = models.ForeignKey(Task)
-
-
-class TaskAssignedTo(models.Model):
-    #log fields
-    creation_date = models.DateTimeField(auto_now_add=True)
-
-    user = models.ForeignKey(User)
-    task = models.ForeignKey(Task)
+    def __str__(self):
+        return self.title
 
 
-class TaskDependency(models.Model):
-    #log fields
-    creation_date = models.DateTimeField(auto_now_add=True)
-
+class TaskDependency(BaseModelDate):
     # This reference points to a Task that needs some other Task to be concluded.
     dependent_task = models.ForeignKey(Task, related_name='dependent_task')
     # This, in turn, points to the specific Task that is holding the conclusion of another one.
@@ -43,24 +63,12 @@ class TaskDependency(models.Model):
             raise AttributeError('A Task cannot depend on itself.')
 
 
-class Comment(models.Model):
-    #log fields
-    creation_date = models.DateTimeField(auto_now_add=True)
-    last_update = models.DateTimeField(auto_now=True)
-
-
-class MainComment(Comment):
+class Comment(BaseModelDate):
+    parent = models.ForeignKey('self', null=True, blank=True)
     title = models.CharField(max_length=200, null=False)
     description = models.TextField(null=False)
     user = models.ForeignKey(User)
     task = models.ForeignKey(Task)
 
-
-class Reply(Comment):
-    comment = models.ForeignKey(MainComment)
-    user = models.ForeignKey(User)
-
-
-class UserCommentTag(models.Model):
-    user = models.ManyToManyField(User)
-    comment = models.ForeignKey(Comment)
+    def __str__(self):
+        return self.title
