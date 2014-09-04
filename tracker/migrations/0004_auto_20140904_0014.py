f25267e545ba943cb58e1e9be28ff68b08f86849
Jonas Borges <jonasborgesalves@gmail.com>
2014-09-04 00:27:05 -0300

    Fix wrong migration

--- tracker/migrations/0004_auto_20140904_0014.py
+++ tracker/migrations/0004_auto_20140904_0014.py
@@ -11,19 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Comment',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('creation_date', models.DateTimeField(auto_now_add=True)),
-                ('last_update', models.DateTimeField(auto_now=True)),
-                ('title', models.CharField(max_length=200)),
-                ('description', models.TextField()),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
         migrations.AlterField(
             model_name='task',
             name='last_update',
