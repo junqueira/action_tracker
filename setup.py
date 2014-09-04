64e531e3015c01ab0778c1c4e66f29adfadb59ba
valdergallo <valdergallo@gmail.com>
2014-09-04 13:53:10 -0300

    add pytest

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 from pip.req import parse_requirements
-import io
 import os
 import sys
 
 pkgname = 'action_tracker'
-version = '0.0.0'
+version = '0.0.1'
 
 
 class PyTest(TestCommand):
@@ -23,8 +22,10 @@ class PyTest(TestCommand):
         errno = pytest.main(self.test_args)
         sys.exit(errno)
 
-install_reqs = parse_requirements('requeriments.txt')
-install_requires = [str(i.req) for i in install_reqs]
+install_requires = [str(i.req) for i in parse_requirements('requeriments.txt')]
+
+install_requires_dev = [str(i.req) for i in parse_requirements('requeriments-dev.txt')]
+
 
 def readme():
     try:
@@ -32,7 +33,7 @@ def readme():
         with open('README.rst') as f:
             return f.read()
     except:
-        return 'Automantic compress files after upload'
+        return 'Project Manager'
 
 
 setup(name=pkgname,
@@ -40,28 +41,28 @@ setup(name=pkgname,
       download_url='https://github.com/nokiadev/action_tracker/tarball/v%s/' % version,
       author="Nokia",
       author_email='valdergallo@gmail.com',
-      keywords=['django', 'manager', 'tracker',],
+      keywords=['django', 'manager', 'tracker'],
       description='Action Tracker',
       long_description=readme(),
-      license='GPL(v3)',
-      classifiers=[
-          'Framework :: Django',
-          'Operating System :: OS Independent',
-          'Topic :: Action Tracker',
-          'Topic :: Project Manager',
-          'Topic :: Internet',
-          'Topic :: Other/Nonlisted Topic',
-          'Topic :: Software Development',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.3',
-      ],
+      license='GNU GPLv3',
+      classifiers=['Framework :: Django',
+                  'Operating System :: OS Independent',
+                  'Topic :: Action Tracker',
+                  'Topic :: Project Manager',
+                  'Topic :: Internet',
+                  'Topic :: Other/Nonlisted Topic',
+                  'Topic :: Software Development',
+                  'License :: OSI Approved :: GNU General Public License (GPL)',
+                  'Programming Language :: Python',
+                  'Programming Language :: Python :: 2.6',
+                  'Programming Language :: Python :: 2.7',
+                  'Programming Language :: Python :: 3.3'],
       include_package_data=True,
       version=version,
       install_requires=install_requires,
-      # tests_require: tests_require,
+      tests_require=install_requires_dev,
       cmdclass={'test': PyTest},
       packages=find_packages(where='.',
-                             exclude=('*test*', '*example*', 'runtest')),
-)
+                             exclude=('*test*', '*example*', 'runtest')
+                             ),
+      )
