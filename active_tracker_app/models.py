1ba2e2e09dc8ad7397c1c2aecbf5f5a28aab38e8
Jonas Borges <jonasborgesalves@gmail.com>
2014-09-03 23:17:34 -0300

    Add Task model #14

--- active_tracker_app/models.py
+++ active_tracker_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    #log fields
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now_add=True)
+
+    #business fields
+    title = models.CharField(max_length=200, null=False)
+    description = models.TextField(null=False)
+    expected_finish_date = models.DateTimeField(auto_now_add=True)
+    finish_date = models.DateTimeField(auto_now=True)
